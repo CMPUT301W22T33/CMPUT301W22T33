aea2e9750f8225fcb9f357405f055198727189bc
andrew-rosario <71676291+andrew-rosario@users.noreply.github.com>
2022-02-01 19:33:24 -0700

    Reset author information

---
+++ app/src/main/java/com/example/qr_code_pursuit/Square.java
@@ -0,0 +1,11 @@
+package com.example.qr_code_pursuit;
+
+public class Square extends Shape{
+    public Square (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){return this.x;}
+    public int getY(){return this.y;}
+}
